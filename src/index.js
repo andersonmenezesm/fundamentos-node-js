8009b6841ae1b93fb1bbee8449f4749e1fbc84e5
Anderson Menezes <anderson.m.menezes@accenture.com>
2021-03-13 13:06:19 -0300

    Update code

--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
-import express from 'express';
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/courses", (request, response) => {
-    const query = request.query;
-    console.log(query);
-    return response.json(["Curso 1","Curso 2","Curso 3"]);
-});
+const customers = [];
 
-app.post("/courses", (request, response) => {
-    const body = request.body;
-    console.log(body);
-    return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
-});///////////////
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
 
-app.put("/courses/:id", (request, response) => {
-    const { id } = request.params;
-    console.log(id);
-    return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
-});
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
 
-app.patch("/courses/:id", (request, response) => {
-    return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
-});
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: 'Customer already exists!' });
+  }
 
-app.delete("/courses/:id", (request, response) => {
-    return response.json(["Curso 6", "Curso 2", "Curso 4"]);
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  console.log(customers);
+  return response.status(201).send();
 });
 
 app.listen(3333);
